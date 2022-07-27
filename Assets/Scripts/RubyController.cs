7e72641e7bad6648a555a351fabae3beac014736
NeverGiveUpToStudy <wuliangjun007@qq.com>
2022-07-27 17:20:49 +0800

    世界添加碰撞组件
    世界添加碰撞组件,让角色的移动更逼真

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,22 +2,28 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour {
 
-
+    private Rigidbody2D rb2D;
+    private float horizontal;
+    private float vertical;
 
     // Start is called before the first frame update
     private void Start() {
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
+
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update() {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y += 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+    private void FixedUpdate() {
+        Vector2 position = rb2D.position;
+        position.x = 3.0f * horizontal * Time.deltaTime + position.x;
+        position.y = 3.0f * vertical * Time.deltaTime + position.y;
+        rb2D.MovePosition(position);
     }
 }
